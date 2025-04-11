b879fa7f4607967f96eed66f91ef96ddeea75fbf
C0mplexity0 <106925362+C0mplexity0@users.noreply.github.com>
2025-04-11 16:52:28 +0100

    feat: Resizable ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,77 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FileAttachment from "./components/home/FileAttachment";
 import MessageStats, { Message, MessageStatsOptions, MessageStatsSettings } from "./components/home/MessageStats";
 import { getDefaultMessageStatsSettings } from "./lib/message-stats-utils";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "./components/ui/resizable";
+
+function Settings({ setMessages, setSettings, settings }: { setMessages: (messages: Message[]) => void, setSettings: (settings: MessageStatsSettings) => void, settings: MessageStatsSettings }) {
+  return (
+    <>
+      <FileAttachment
+        onChange={async (files) => {
+          const file = files[0];
+
+          if (!file) {
+            return;
+          }
+
+          const content = JSON.parse(await file.text());
+          
+          setMessages(content);
+        }}
+      />
+
+      <MessageStatsOptions
+        onUpdate={(key: keyof MessageStatsSettings, value) => {
+          const newSettings = { ...settings, [key]: value };
+          setSettings(newSettings);
+        }}
+      />
+    </>
+  );
+}
 
 export default function App() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [settings, setSettings] = useState<MessageStatsSettings>(getDefaultMessageStatsSettings());
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-  return (
-    <main className="size-full flex flex-col md:flex-row gap-5">
-      <div className="p-5 pb-0 md:pb-5 grow-0 shrink-0 md:basis-80 lg:basis-100 md:pr-0">
-        <div className="border rounded-xl w-full md:h-full p-5">
-          <FileAttachment
-            onChange={async (files) => {
-              const file = files[0];
+  useEffect(() => {
+    const listener = () => {
+      setScreenWidth(window.innerWidth);
+    };
 
-              if (!file) {
-                return;
-              }
+    window.addEventListener("resize", listener);
 
-              const content = JSON.parse(await file.text());
-              
-              setMessages(content);
-            }}
-          />
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, [screenWidth]);
 
-          <MessageStatsOptions
-            onUpdate={(key: keyof MessageStatsSettings, value) => {
-              const newSettings = { ...settings, [key]: value };
-              setSettings(newSettings);
-            }}
-          />
-        </div>
+  return screenWidth < 800 ?
+  (
+    <main className="size-full flex flex-col gap-5">
+      <div className="p-5 pb-0 grow-0 shrink-0">
+        <Settings setMessages={setMessages} setSettings={setSettings} settings={settings} />
       </div>
 
-      <div className="overflow-auto p-5 pt-0 md:pt-5 md:pl-0">
+      <div className="overflow-auto p-5 pt-0">
         <MessageStats messages={messages} settings={settings} />
       </div>
     </main>
   )
+  :
+  (
+    <ResizablePanelGroup direction="horizontal" className="size-full">
+      <ResizablePanel minSize={25} className="p-5">
+        <Settings setMessages={setMessages} setSettings={setSettings} settings={settings} />
+      </ResizablePanel>
+
+      <ResizableHandle withHandle />
+
+      <ResizablePanel minSize={50} className="p-5">
+        <MessageStats messages={messages} settings={settings} />
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
 }
